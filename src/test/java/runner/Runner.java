9b3c51537616fa76802ce3ce2c5f631277e6ef88
kanysaiorozbekkyzy <k.orozbekkyzy0707@gmail.com>
2021-09-03 18:12:45 -0500

    Automation for Newsletter subscription functionality validation.

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         plugin={"json:target/cucumber.json","rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@MB2P-126",
+        tags = "@MB2P-214",
         dryRun = false
 
 )
